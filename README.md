6dcc15dbd69e55658af820d924aa2aa36de236c2
Morteza Tavanarad <mory0tiki@gmail.com>
2014-12-31 15:30:53 +0330

    adding string format

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ js-string-format
 ================
 
 Format a js string like C#
+
+##How To Use:
+```
+var str = "Hello {0}";
+str.format('World!");
+console.log(str);
+```
+
---
+++ mtjs-string-format.js
@@ -0,0 +1,8 @@
+String.prototype.format = function () {
+    var s = this;
+    for (var i = 0; i < arguments.length; i++) {
+        var reg = new RegExp("\\{" + i + "\\}", "gm");
+        s = s.replace(reg, arguments[i]);
+    }
+    return s;
+}
