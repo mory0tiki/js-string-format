fbc4516798d749508a8420501222bf9c9948bcbb
Morteza Tavanarad <mory0tiki@gmail.com>
2014-12-31 15:47:07 +0330

    Change to IIFE

--- mtjs-string-format.js
+++ mtjs-string-format.js
@@ -1,8 +1,11 @@
-String.prototype.format = function () {
-    var s = this;
-    for (var i = 0; i < arguments.length; i++) {
-        var reg = new RegExp("\\{" + i + "\\}", "gm");
-        s = s.replace(reg, arguments[i]);
+(function(){
+    'use strict';
+    String.prototype.format = function () {
+        var s = this;
+        for (var i = 0; i < arguments.length; i++) {
+            var reg = new RegExp("\\{" + i + "\\}", "gm");
+            s = s.replace(reg, arguments[i]);
+        }
+        return s;
     }
-    return s;
-}
+})();
